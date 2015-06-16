1c1cb7446ed8413bb5f312c39ef0ed006e28288c
RogerOlasz <rogerolasz96@gmail.com>
2015-06-16 12:29:46 +0200

    Changes

---
+++ FinalExamFiles/FinalExamFiles/P2String.h
@@ -0,0 +1,309 @@
+#ifndef __P2String_H__
+#define __P2String_H__
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+#define TMP_STRING_SIZE 4096
+
+class P2String
+{
+private:
+		unsigned int len;
+		char* str;
+
+		void Alloc(unsigned int memory_space)
+		{
+			len = memory_space;
+			str = new char[len];
+		}
+
+public:
+	
+
+	P2String()
+	{
+		Alloc(1);
+		clear();
+		
+	}
+	/*P2String(const char* strc)
+	{
+		len = strlen(strc) + 1;
+		str = new char[len];
+		strcpy_s(str, len, strc);
+	}*/
+	P2String(const P2String& strc)
+	{
+		len = strc.len;
+		str = new char[len];
+		strcpy_s(str, len, strc.str);
+	}
+
+	P2String(const char* format, ...)
+	{
+		len = 0;
+
+		if (format != NULL)
+		{
+			static char tmp[TMP_STRING_SIZE];
+			static va_list ap;
+
+			va_start(ap, format);
+			int res = vsprintf_s(tmp, TMP_STRING_SIZE, format, ap);
+			va_end(ap);
+
+			if (res > 0)
+			{
+				Alloc(res + 1);
+				strcpy_s(str, len, tmp);
+				
+			}
+		if (len == 0)
+		{
+			Alloc(1);
+			clear();
+		}
+
+		}
+	}
+
+	~P2String()
+	{
+		delete[] str;
+	}
+
+	char* getString() const
+	{
+		return str;
+	}
+
+	unsigned int capacity() const
+	{
+		return len;
+	}
+
+	unsigned int getLength() const
+	{
+		return strlen(str);
+	}
+
+	bool Cut(unsigned int start, unsigned int end)
+	{
+		unsigned int size = getLength();
+
+		if (end == 0 || end >= getLength())
+		{
+			end =size - 1;
+		}
+
+		if (start > size || end <= start)
+		{
+			return false;
+		}
+
+		char* begin = str + start;
+		char* endless = str + end + 1;
+
+		while (*begin++ = *endless++);
+
+		return true;
+
+	}
+
+	void Trim()
+	{
+		unsigned int size = getLength();
+
+		char* end = str + size;
+		while (*--end == ' ') *end = '\0';
+
+		char* start = str;
+		while (*++start == ' ');
+
+		size = strlen(start);
+
+		for (unsigned int i = 0; i < size + 1; i++)
+		{
+			str[i] = start[i];
+		}
+	}
+
+	bool SwapChar(char* c1, char* c2)
+	{
+		int start = 0;
+		int end = -1;
+		int dif = strlen(c2) - strlen(c1);
+		int oldLength = getLength();
+
+		//Find the word
+		for (unsigned int i = 0; i < getLength(); i++)
+		{
+			if (str[i] == c1[0])
+			{
+				start = i;
+				if (strlen(c1) == 1)
+				{
+					end = i;
+				}
+					
+				else
+				{
+					for (unsigned int j = 0; str[i + 1] == c1[j], j < strlen(c1); j++, i++)
+					{
+						if (j == strlen(c1) - 1)
+						{
+							end = i + 1;
+						}		
+					}
+				}					
+			}
+		}	
+
+		//Doesn't exist
+		if (end == -1)
+		{
+			return false;
+		}
+			
+		//Resize
+		if (dif > 0)
+		{
+			char* tmp = str;
+
+			Alloc(len + dif);
+			strcpy_s(str, getLength(), tmp);
+
+			for (int i = oldLength; i > end; i--)
+			{
+				str[i + dif] = str[i];
+			}
+				
+		}
+		else
+		{
+			for (int i = end + 1; i < oldLength; i++)
+			{
+				str[i + dif] = str[i];
+			}
+			
+			str[oldLength + dif] = '\0';
+			len += dif;
+		}
+		//Rewrite
+		for (int i = 0; i < strlen(c2); i++)
+		{
+			str[i + start] = c2[i];
+		}
+			
+		return true;
+	}
+
+	bool operator== (const P2String& string) const
+	{
+		return strcmp(str, string.str) == 0;
+	}
+
+	bool operator== (const char* string) const
+	{
+		if (string != NULL)
+			return strcmp(str, string) == 0;
+		return false;
+	}
+
+	bool operator!= (const P2String& string) const
+	{
+		return strcmp(str, string.str) != 0;
+	}
+
+	bool operator!= (const char* string) const
+	{
+		if (string != NULL)
+			return strcmp(str, string) != 0;
+		return true;
+	}
+
+	const P2String& operator= (const P2String& string) 
+	{
+		str = new char[string.len];
+		strcpy_s(str, string.len, string.str);
+
+		return(*this);
+	}
+
+	const P2String& operator= (const char* string)
+	{
+		if (string != NULL)
+		{
+			if (strlen(string) + 1 > len)
+			{
+				delete[] str;
+				Alloc(strlen(string) + 1);
+			}
+
+			else
+
+			strcpy_s(str, len, string);
+		}
+		else
+		{
+			clear();
+		}
+
+		return (*this);
+
+	}
+
+	const P2String& P2String::operator+= (const P2String &string)
+	{
+
+		char *str_tmp = str;
+		unsigned int len_tmp = len;
+		len = string.len + len_tmp;
+
+		char *tmp;
+		tmp = new char[len];
+		strcpy_s(tmp, len, str_tmp);
+		strcat_s(tmp, len, string.str);
+
+		delete[] str;
+		str = tmp;
+
+		return (*this);
+	}
+
+	const P2String& P2String::operator+= (const char *string)
+	{
+		if (string != NULL)
+		{
+			char *str_tmp = str;
+			unsigned int len_tmp = len;
+			len = strlen(string) + len_tmp + 1;
+
+			char *tmp;
+			tmp = new char[len];
+			strcpy_s(tmp, len, str_tmp);
+			strcat_s(tmp, len, string);
+
+			delete[] str;
+			str = tmp;
+		}
+		else
+		{
+			clear();
+		}
+			
+
+		return (*this);
+	}
+
+	void clear()
+	{
+		str[0] = '\0';
+	}
+
+};
+
+#endif //__P2String_H__
---
+++ FinalExamFiles/FinalExamFiles/Tree.h
@@ -0,0 +1,348 @@
+#ifndef __Tree_H__
+#define __Tree_H__
+
+#include "DList.h"
+#include "Stack.h"
+
+//Class TreeNodes from Tree class
+template<class TreeData>
+class TreeNode
+{
+
+public:
+
+	TreeData data;
+	TreeNode* parent;
+	DList<TreeNode*> children;
+
+public:
+
+	TreeNode(const TreeData& new_data)
+	{
+		data = new_data;
+		parent = NULL;
+	}
+
+	void AddChild(TreeNode* node)
+	{
+		children.Add(node);
+		node->parent = this;
+	}
+
+	bool RemoveChild(TreeNode* node)
+	{
+		bool ret = false;
+		DNode<TreeNode*>* item = children.getFirst();
+
+		for (; item != NULL; item = item->next)
+		{
+			TreeNode* son = item->data;
+
+			if (node == son)
+			{
+				children.Del(item);
+				node->parent = NULL;
+				ret = true;
+				break;
+			}
+		}
+
+	return ret;
+	}
+
+	TreeData CalcAddition()
+	{
+		DNode<TreeNode*>* item = children.getFirst();
+		TreeData result = data;
+
+		for (; item != NULL; item = item->next)
+		{
+			TreeNode *child = item->data;
+
+			result += child->CalcAddition();
+		}
+		return result;
+	}
+
+	void PreOrderRecursive(DList<TreeNode<TreeData>*>* list)
+	{
+		list->Add(this);
+
+		DNode<TreeNode*>* item = children.getFirst();
+
+		for (; item != NULL; item = item->next)
+		{
+			item->data->PreOrderRecursive(list);
+		}
+			
+	}
+
+	void PostOrderRecursive(DList<TreeNode<TreeData>*>* list)
+	{
+		DNode<TreeNode*>* item = children.getFirst();
+
+		for (; item != NULL; item = item->next)
+		{
+			item->data->PostOrderRecursive(list);
+		}
+			
+		list->Add(this);
+	}
+
+	void InOrderRecursive(DList<TreeNode<TreeData>*>* list) //Leave odd TreeNode on right
+	{
+		DNode<TreeNode*>* item = children.getFirst();
+		unsigned int mid = children.Count() / 2;
+
+		for (unsigned int i = 0; i < mid; i++, item = item->next)
+		{
+			item->data->InOrderRecursive(list);
+		}
+			
+		list->Add(this);
+
+		for (; item != NULL; item = item->next)
+		{
+			item->data->InOrderRecursive(list);
+		}	
+	}
+
+	TreeNode<TreeData>* FindRecursive(const TreeData& node) //To find any TreeNode
+	{
+		if (node == data)
+		{
+			return this;
+		}
+			
+		TreeNode<TreeData>* result = NULL;
+		DNode<TreeNode*>* item = children.getFirst();
+
+		for (; item != NULL; item = item->next)
+		{
+			TreeNode* son = item->data;
+			result = son->FindRecursive(node);
+
+			if (result != NULL)
+			{
+				break;
+			}	
+		}
+	return result;
+	}
+
+	void CollectAll(DList<TreeNode*>* list) //To collect all TreeNodes into list
+	{
+		list->Add(this);
+
+		DNode<TreeNode*>* item = children.getFirst();
+
+		for (; item != NULL; item = item->next)
+		{
+			TreeNode* son = item->data;
+			son->CollectAll(list);
+		}
+	}
+
+	void CollectAllData(DList<TreeData>* list)//To collect all TreeNodes data into list
+	{
+		list->Add(data);
+
+		DNode<TreeNode*>* item = children.getFirst();
+
+		for (; item != NULL; item = item->next)
+		{
+			TreeNode* son = item->data;
+			son->CollectAllData(list);
+		}
+	}
+
+};
+
+//Tree class 
+template<class TreeData>
+class Tree
+{
+public:
+
+	TreeNode<TreeData> root_node;
+
+public:
+
+	Tree(const TreeData& new_data) : root_node(new_data)
+	{}
+
+	~Tree()
+	{}
+	//Exam Methods
+	TreeData CalcAdditionRecursive()
+	{
+		return root_node.CalcAddition();
+	}
+	TreeData CalcAdditionIterative()
+	{
+		TreeData result = 0;
+		Stack<TreeNode<TreeData>*> stack;
+		TreeNode<TreeData>* node = &root_node;
+
+		while (node != NULL || stack.Pop(node))
+		{
+			result += node->data;
+			DNode<TreeNode<TreeData>*>* item = node->children.getLast();
+
+			for (; item != node->children.getFirst(); item = item->previous)
+			{
+				stack.Push(item->data);
+			}
+
+			if (item != NULL)
+			{
+				node = item->data;
+			}
+			else
+			{
+				node = NULL;
+			}
+		}
+		return result;
+	}
+
+
+	//Recursive methods
+	void PreOrderRecursive(DList<TreeNode<TreeData>*>* list)
+	{
+		root_node.PreOrderRecursive(list);
+	}
+
+	void PostOrderRecursive(DList<TreeNode<TreeData>*>* list)
+	{
+		root_node.PostOrderRecursive(list);
+	}
+
+	void InOrderRecursive(DList<TreeNode<TreeData>*>* list)
+	{
+		root_node.InOrderRecursive(list);
+	}
+
+	//Iterative methods
+	void PreOrderIterative(DList<TreeNode<TreeData>*>* list)
+	{
+		Stack<TreeNode<TreeData>*> stack;
+		TreeNode<TreeData>* node = &root_node;
+
+		while (node != NULL || stack.Pop(node))
+		{
+			list->Add(node);
+			DNode<TreeNode<TreeData>*>* item = node->children.getLast();
+
+			for (; item != node->children.getFirst(); item = item->previous)
+			{
+				stack.Push(item->data);
+			}
+			
+			if (item != NULL)
+			{
+				node = item->data;
+			}
+			else
+			{
+				node = NULL;
+			}
+		}
+	}
+
+	void PostOrderIterative(DList<TreeNode<TreeData>*>* list)
+	{
+		Stack<TreeNode<TreeData>*> stack;
+		TreeNode<TreeData>* node = &root_node;
+
+		while (node != NULL || stack.Pop(node))
+		{
+			DNode<TreeNode<TreeData>*>* item = node->children.getLast();
+
+			if (item != NULL && list->Find(item->data) == -1)
+			{
+				stack.Push(node);
+
+				for (; item != node->children.getFirst(); item = item->previous)
+				{
+					stack.Push(item->data);
+				}
+
+				node = item->data;
+			}
+			else
+			{
+				list->Add(node);
+				node = NULL;
+			}
+		}
+	}
+
+	void InOrderIterative(DList<TreeNode<TreeData>*>* list)
+	{
+		Stack<TreeNode<TreeData>*> stack;
+		TreeNode<TreeData>* node = &root_node;
+
+		while (node->children.Count != NULL)
+		{
+			DNode<TreeNode<TreeData>*>* item = node->children.getFirst();
+			unsigned int mid = node->children.Count() / 2;
+
+			for (unsigned int i = 0; i < mid; i++, item = item->next)
+			{
+				stack.Push(item);
+				node = item;
+			}
+		}
+
+		stack.Pop(node);
+		list->Add(node);
+		node = node->parent;
+
+		while (node->children.Count != NULL)
+		{
+			DNode<TreeNode<TreeData>*>* item = node->children.getLast();
+			unsigned int mid = node->children.Count() / 2;
+
+			for (unsigned int i = 0; i < mid; i++, item = item->next)
+			{
+				stack.Push(item);
+				node = item;
+			}
+		}
+	}
+
+	void Add(const TreeData& data)
+	{
+		TreeNode<TreeData>* new_node = new TreeNode<TreeData>(data);
+		root_node.AddChild(new_node);
+	}
+
+	void Add(const TreeData& data, const TreeData& parent)
+	{
+		TreeNode<TreeData>* dad = root_node.FindRecursive(parent);
+		TreeNode<TreeData>* new_node = new TreeNode<TreeData>(data);
+		dad->AddChild(new_node);
+	}
+
+	void Clear()
+	{
+		DList<TreeNode<TreeData>*> nodes_deleted;
+		root_node.CollectAll(&nodes_deleted);
+
+		DNode<TreeNode<TreeData>*>* item = nodes_deleted.getFirst();
+
+		for (; item != NULL; item = item->next)
+		{
+			TreeNode<TreeData>* son = item->data;
+
+			if (son->parent)
+			{
+				son->parent->RemoveChild(son);
+			}	
+		}
+	}
+
+};
+
+#endif // __Tree_H__
